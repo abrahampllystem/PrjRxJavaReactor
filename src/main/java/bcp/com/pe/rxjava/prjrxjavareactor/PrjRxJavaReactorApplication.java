ca780dfef3e39c917d694e645d2b47b129035ca6
abraham peralta <abraham20467k@gmail.com>
2023-06-10 09:48:12 -0500

    second commit

---
+++ src/main/java/bcp/com/pe/rxjava/prjrxjavareactor/PrjRxJavaReactorApplication.java
@@ -0,0 +1,13 @@
+package bcp.com.pe.rxjava.prjrxjavareactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjRxJavaReactorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrjRxJavaReactorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bcp/com/pe/rxjava/prjrxjavareactor/PrjRxJavaReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package bcp.com.pe.rxjava.prjrxjavareactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjRxJavaReactorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
